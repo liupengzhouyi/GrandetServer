4d5ae5e156565b2874432d4871ba957fe3aa0dab
liupeng <liupeng.0@outlook.com>
2023-10-17 09:04:53 +0800

    fix: Add setting file.

---
+++ app/settings.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#!coding=utf-8
+
+import os
+
+MySQL_ip = '127.0.0.1'
+MySQL_user = 'root'
+MySQL_password = "12345678"
+
+app_root_path = os.path.dirname(os.path.abspath(__file__))
+upload_folder = "grander_folder"
+log_file_path = "./../log/app1.log"
+
+bill_No1_line = "交易时间"
+
